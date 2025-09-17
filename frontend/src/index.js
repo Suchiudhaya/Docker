e19849bdadf36a90e43f81303235e69e12160bcf
Suchiudhaya <gratitudepourtout@gmail.com>
2025-09-17 11:50:03 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // imports the default export
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);

33c23ee4e437b60878d74eb3f826bf5477cd2a0b
Suchiudhaya <gratitudepourtout@gmail.com>
2025-09-17 14:18:51 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import UsersList from "./components/UsersList";
+import StatusList from "./components/StatusList";
 
-function App() {
+const App = () => {
   const [users, setUsers] = useState([]);
-  const [status, setStatus] = useState({});
+  const [status, setStatus] = useState([]);
 
   useEffect(() => {
-    fetch('http://users-service:5001/users')
-      .then(res => res.json())
-      .then(data => setUsers(data))
-      .catch(err => console.error(err));
-
-    fetch('http://status-service:5002/status')
-      .then(res => res.json())
-      .then(data => setStatus(data))
-      .catch(err => console.error(err));
+    axios.get("/users").then(res => setUsers(res.data)).catch(err => console.error(err));
+    axios.get("/status").then(res => setStatus(res.data)).catch(err => console.error(err));
   }, []);
 
   return (
     <div>
-      <h1>DevOps React App</h1>
-      <h2>Status</h2>
-      <pre>{JSON.stringify(status, null, 2)}</pre>
-      <h2>Users</h2>
-      <ul>
-        {users.map(u => <li key={u.id}>{u.name}</li>)}
-      </ul>
+      <h1>Users</h1>
+      <UsersList users={users} />
+      <h1>Status</h1>
+      <StatusList status={status} />
     </div>
   );
-}
+};
 
 export default App;

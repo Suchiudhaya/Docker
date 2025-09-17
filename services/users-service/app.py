8d754b8760525009fa5fe8b432d2c27ed0283be6
Suchiudhaya <gratitudepourtout@gmail.com>
2025-09-17 14:20:51 +0100

    Update app.py

--- services/users-service/app.py
+++ services/users-service/app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/users")
-def get_users():
+def users():
     return jsonify([
         {"id": 1, "name": "Alice"},
         {"id": 2, "name": "Bob"}
